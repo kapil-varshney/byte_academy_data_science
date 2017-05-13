fa57ad426fa1c13625a7f679fb585d5dc55116c3
Kenso Trabing <kenso.trabing@bloomwebsite.com>
2017-05-12 23:10:01 -0400

    Rebuild repository

---
+++ rebuild.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cp toolkit/build_track.py .
+python3 build_track.py
+rm build_track.py
--- toolkit/build_track.py
+++ toolkit/build_track.py
@@ -2,22 +2,40 @@
 
 import os
 
+
 # Setting
 
-weeks_in_track = 12
-days_in_week = 5
+default_weeks_in_track = 12
+default_days_in_week   = 5
+
 
 # Scripting
 
-for week in range(weeks_in_track):
+for week in range(default_weeks_in_track):
     week += 1
-    os.system('mkdir week_{0}'.format(week))
-    os.system('touch week_{0}'.format(week))
-    os.system('echo "# Week {0}" >> week_{0}/README.md'.format(week))
+
+    if week < 10:
+
+        os.system('mkdir week_0{0}'.format(week))
+        os.system('touch week_0{0}'.format(week))
+        os.system('echo "# Week 0{0}" >> week_0{0}/README.md'.format(week))
+
+        for day in range(default_days_in_week):
+            day += 1
+
+            os.system('mkdir week_0{0}/day_{1}'.format(week, day))
+            os.system('touch week_0{0}/day_{1}/README.md'.format(week, day))
+            os.system('echo "# Week 0{0}, Day {1}" >> week_0{0}/day_{1}/README.md'.format(week, day))
 
 
-    for day in range(days_in_week):
-        day += 1
-        os.system('mkdir week_{0}/day_{1}'.format(week, day))
-        os.system('touch week_{0}/day_{1}/README.md'.format(week, day))
-        os.system('echo "# Day {0}" >> week_{0}/day_{1}/README.md'.format(week, day))
+    else:
+        os.system('mkdir week_{0}'.format(week))
+        os.system('touch week_{0}'.format(week))
+        os.system('echo "# Week {0}" >> week_{0}/README.md'.format(week))
+
+        for day in range(default_days_in_week):
+            day += 1
+
+            os.system('mkdir week_{0}/day_{1}'.format(week, day))
+            os.system('touch week_{0}/day_{1}/README.md'.format(week, day))
+            os.system('echo "# Week {0}, Day {1}" >> week_{0}/day_{1}/README.md'.format(week, day))
